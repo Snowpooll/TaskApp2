545fc069e7264a904c1abae913312ebba402d7b0
snowpool <snowpoollovely@gmail.com>
2017-01-12 23:06:17 +0900

    adapter add

--- app/src/main/java/jp/techacademy/kubota/satoru/taskapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/kubota/satoru/taskapp/MainActivity.java
@@ -8,15 +8,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView mListView;
+    private TaskAdapter mTaskAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,27 +30,42 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //listview 設定
+        mTaskAdapter = new TaskAdapter(MainActivity.this);
+        mListView = (ListView)findViewById(R.id.listView1);
+
+        //listタップイベント
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent , View view, int position, long id) {
+                //入力、編集画面に遷移
+            }
+        });
+
+        //list 長押し処理
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                //task 削除
+                return true;
+            }
+        });
+
+        reloadListView();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void reloadListView(){
+        //後でtaskに変更予定
+        ArrayList<String> taskArrayList = new ArrayList<>();
+//        taskArrayList.add("aaa");
+//        taskArrayList.add("bbb");
+//        taskArrayList.add("ccc");
+
+        mTaskAdapter.setTaskArrayList(taskArrayList);
+        mListView.setAdapter(mTaskAdapter);
+        mTaskAdapter.notifyDataSetChanged();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
